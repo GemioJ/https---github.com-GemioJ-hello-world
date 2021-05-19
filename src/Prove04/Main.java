351350e48202e52c73091a5c33e4f68f50070083
GemioJ <gemioj@byui.edu>
2021-05-19 13:49:56 -0400

    This is my original file

--- src/Prove04/Main.java
+++ src/Prove04/Main.java
@@ -9,7 +9,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String name = "John";
+        String name = "John Smith";
         System.out.println("Hello, my name is: " + name);
 
     }
