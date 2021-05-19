f13e082bc887e10569f8b67831cf8d7043025ccd
GemioJ <gemioj@byui.edu>
2021-05-19 13:25:03 -0400

    This is my original file

---
+++ src/Prove04/Main.java
@@ -0,0 +1,15 @@
+package Prove04;
+
+public class Main {
+    String name;
+
+
+    public Main(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        String name = "Jeannette";
+        System.out.println("Hello, my name is: " + name);
+    }
+}
