9d245bd128d8d414d0e6b5c333a19fb52e098d6c
GemioJ <84032781+GemioJ@users.noreply.github.com>
2021-05-21 22:49:57 -0400

    Update Main.java

--- src/Prove04/Main.java
+++ src/Prove04/Main.java
@@ -2,15 +2,20 @@ package Prove04;
 
 public class Main {
     String name;
+    String teamM;
 
 
-    public Main(String name){
+    public Main(String name, String teamM){
         this.name = name;
+        this.teamM = teamM;
+        
     }
 
     public static void main(String[] args) {
         String name = "John Smith";
+        String teamM = "";
         System.out.println("Hello, my name is: " + name);
+        System.out.println("Hello, my name is: " + teamM); 
 
     }
 }
