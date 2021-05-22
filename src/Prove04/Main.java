e9c781ba3bb82082da42ca300601a2efde3ad0d2
GemioJ <gemioj@byui.edu>
2021-05-22 19:07:46 -0400

    This is a new one

--- src/Prove04/Main.java
+++ src/Prove04/Main.java
@@ -1,21 +1,13 @@
 package Prove04;
 
 public class Main {
-    String name;
-    String teamM;
 
-
-    public Main(String name, String teamM){
-        this.name = name;
-        this.teamM = teamM;
-        
-    }
 
     public static void main(String[] args) {
         String name = "Jeannette";
-        String teamM = "";
-        System.out.println("Hello, my name is: " + name);
-        System.out.println("Hello, my name is: " + teamM); 
+
+        System.out.println("Hello team, my name is: " + name);
+
 
     }
 }
