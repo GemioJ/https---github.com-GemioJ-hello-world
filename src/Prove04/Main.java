b5682e4ea6656f3aabcd6e1aac7260473cd6a7b6
GemioJ <gemioj@byui.edu>
2021-05-21 23:00:15 -0400

    I added a new code in GitHub

--- src/Prove04/Main.java
+++ src/Prove04/Main.java
@@ -12,7 +12,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String name = "John Smith";
+        String name = "Jeannette";
         String teamM = "";
         System.out.println("Hello, my name is: " + name);
         System.out.println("Hello, my name is: " + teamM); 
